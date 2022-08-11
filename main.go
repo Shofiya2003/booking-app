1dc18d84d5680e561d34768f8ee9c6af958c483d
Shofiya2003 <shofiyabootwala@gmail.com>
2022-08-12 02:22:32 +0530

    learnt arrays, slices and loops

--- main.go
+++ main.go
@@ -2,25 +2,45 @@
 package main
 
 //Importing Format package from GO library
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const totalTickets = 50
 	//specifying types after the variable name
 	var remainingTickets uint = 50
-	fmt.Printf("Welcome to %v booking app\n", conferenceName)
-	fmt.Printf("We have only %v tickets avalaible. Hurry!\n", remainingTickets)
-	fmt.Printf("You can book tickets for %v here\n", conferenceName)
+	//slice for all bookings
+	var bookings []string
 	var username string
 	var userTickets uint
 
-	//taking user input
-	fmt.Printf("Enter your name\n")
-	fmt.Scan(&username)
+	fmt.Printf("Welcome to %v booking app\n", conferenceName)
+	fmt.Printf("We have only %v tickets avalaible. Hurry!\n", remainingTickets)
+	fmt.Printf("You can book tickets for %v here\n", conferenceName)
 
-	fmt.Printf("Enter number of tickets you want to book\n")
-	fmt.Scan(&userTickets)
+	for {
 
-	fmt.Printf("User %v booked %v tickets", username, userTickets)
+		//taking user input
+		fmt.Printf("Enter your name\n")
+		fmt.Scan(&username)
+
+		fmt.Printf("Enter number of tickets you want to book\n")
+		fmt.Scan(&userTickets)
+
+		remainingTickets -= userTickets
+		bookings = append(bookings, username)
+		fmt.Printf("User %v booked %v tickets\n", username, userTickets)
+		fmt.Printf("There are %v tickets left\n", remainingTickets)
+		var firstNames []string
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("All are bookings are %v\n", firstNames)
+	}
+
 }
